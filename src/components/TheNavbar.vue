ad37be6d2f6530d5b5142ceed3cdc0dac8193097
Raquel Martínez <raquel.mrti@gmail.com>
2023-06-18 20:53:21 +0200

    add list db and GameListCreate component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <img src="http://via.placeholder.com/640x360" alt="" />
+  <h1>Playshelf</h1>
+  <h2>A simple videogame list builder</h2>
+  <p>
+    Organize your favorite titles into personalized lists quickly and easily. Playshelf lets you
+    build, share, and explore game collections like never before.
+  </p>
+
+  <button>Get started</button>
+</template>
